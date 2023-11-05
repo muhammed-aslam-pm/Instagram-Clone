ed981d3b951e2be8c0cb25ba2ffd0dd57cac425e
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-11-05 14:43:57 +0530

    bottom nav bar created

---
+++ lib/utils/color_constant/color_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color primaryBlack = Color(0xff000000);
+}
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/color_constant/color_constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +9,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: selectedIndex,
+          selectedIconTheme: IconThemeData(
+              color: ColorConstants.primaryBlack, size: 30, fill: 0.1),
+          unselectedIconTheme: IconThemeData(
+              color: ColorConstants.primaryBlack, size: 30, fill: 0.1),
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          onTap: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  selectedIndex == 0 ? Icons.home : Icons.home_outlined,
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  size: selectedIndex == 1 ? 35 : 30,
+                ),
+                label: "search"),
+            const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_box_outlined,
+                ),
+                label: "Add"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  selectedIndex == 3 ? Icons.favorite : Icons.favorite_border,
+                ),
+                label: "fav"),
+            const BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  radius: 13,
+                ),
+                label: "fav"),
+          ]),
+    );
   }
 }
